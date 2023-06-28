97bc09968c83862038d805a9401a2bc41ce7d86f
Kcinnay_23 <yatadegnon2002@gmail.com>
2023-06-28 13:23:09 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Iris Flower Prediction App""")
